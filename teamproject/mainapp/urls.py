3c2fc489ef506a8de601ff65ae3d5c9c94ef0372
JuHee Kim <rlawngml59@gmail.com>
2022-10-14 13:51:30 +0900

    appfordog.app
    - footer 변경
    - QnA 위치 변경(section:cta로)
    - navvar변경
    - QnA를 Log In으로, Log In을 Sign In으로 변경

--- teamproject/mainapp/urls.py
+++ teamproject/mainapp/urls.py
@@ -5,4 +5,11 @@ app_name='mainapp'
 
 urlpatterns = [
     path('', mainpage, name='mainpage'),
+    path('sort_m1_1/', sort_m1_1, name='sort_m1_1'),
+    path('sort_m1_2/', sort_m1_2, name='sort_m1_2'),
+    path('sort_m1_3/', sort_m1_3, name='sort_m1_3'),
+    path('sort_m1_4/', sort_m1_4, name='sort_m1_4'),
+    path('sort_m1_5/', sort_m1_5, name='sort_m1_5'),
+    path('sort_r2/', sort_r2, name='sort_r2'),
+    path('sort_m2/', sort_m2, name='sort_m2'),
 ]
