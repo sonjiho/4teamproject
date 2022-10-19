5a9d9e4f77525217cce099ad53845801acde05ff
JuHee Kim <rlawngml59@gmail.com>
2022-10-19 14:09:18 +0900

    appfordog.app
    - qna 수정폼 작성중

--- teamproject/qnaapp/urls.py
+++ teamproject/qnaapp/urls.py
@@ -4,8 +4,11 @@ from . import views
 app_name='qnaapp'
 
 urlpatterns = [
-    path('', views.qna, name='qna'),
+    path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
     path('question/create/', views.question_create, name='question_create'),
+    path('question/modify/<int:question_id>/', views.question_modify, name="question_modify"),
+    path('question/delete/<int:question_id>/', views.question_delete,  name='question_delete'),
+    path('answer/modify/<int:answer_id>/', views.answer_modify, name="answer_modify"),
 ]
--- teamproject/qnaapp/views.py
+++ teamproject/qnaapp/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Question
+from .models import Question, Answer
 from django.utils import timezone
 from .forms import QuestionForm
+from django. contrib.auth.decorators import login_required
+from django. contrib import messages
 from django.db import models
+from django.core.paginator import Paginator
 
-
-def qna(request):
-    """ 목록 출력"""
-    question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
-    return render(request, 'dogapp/qna_list.html', context)
-
-
+@login_required(login_url='common:login')
 def detail(request, question_id):
     """질문 내용 출력"""
     question = get_object_or_404(Question, pk=question_id)
@@ -20,13 +16,14 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'dogapp/qna_detail.html', context)
 
-
+@login_required(login_url='common:login')
 def question_create(request):
     """질문 등록"""
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('qnaapp:qna')
@@ -36,11 +33,88 @@ def question_create(request):
     print(form)
     return render(request, 'dogapp/qna_form.html', context)
 
-
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
 
     """질문 답변 등록"""
     question= get_object_or_404(Question, pk=question_id)
+    question.author = request.user
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('qnaapp:detail', question_id=question.id)
 
+def index(request):
+    """"목록출력"""
+    page = request.GET.get('page', '1')
+    question_list = Question.objects.order_by('-create_date')
+    paginator = Paginator(question_list, 5)
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
+    return render(request, 'dogapp/qna_list.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    """질문 수정"""
+    question = get_object_or_404(Question, pk=question_id)
+    if request. user != question.author:
+        messages.error(request, '수정 권한이 없으시개!')
+        return redirect('qua:detail', question_id=question.id)
+
+    if request.method =="POST":
+        form=QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('qna:detail', question_id=question.id)
+        else:
+            form = QuestionForm(instance=question)
+        context = {
+            'form': form
+        }
+        return render(request, ' dogapp/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    """질문 삭제"""
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제할 권한이 없개!')
+        return redirect('qna:detail', question_id=question .id)
+    question.delete()
+    return redirect('qna:index')
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    """답변 수정"""
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정 권한이 없으시개!')
+        return redirect('qua:detail', question_id=answer.question.id)
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('qna:detail', question_id=answer.question.id)
+        else:
+            form = AnswerForm(instance=answer)
+        context = {
+            'answer': answer,
+            'form': form
+        }
+        return render(request, ' dogapp/question_form.html', context)
+
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    """답변 삭제"""
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '삭제할 권한이 없개!')
+        return redirect('qna:detail', answer_id=answer.id)
+    answer.delete()
+    return redirect('qna:index')
