b574b14d466f0957744818307810260b84017524
JH <gw05028@naver.com>
2022-10-20 15:19:41 +0900

    알 수 없는 커밋

--- teamproject/qnaapp/forms.py
+++ teamproject/qnaapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Question, Answer, Comment
+from .models import Question, Answer
 
 class QuestionForm(forms.ModelForm):
     class Meta:
@@ -16,13 +16,4 @@ class AnswerForm(forms.ModelForm):
         fields = ['content']
         labels = {
             'content': '답변내용',
-        }
-
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['content']
-        labels = {
-            'content': '댓글내용'
         }
