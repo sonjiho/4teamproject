1da0c784807b4f6555c8af8bf97064628492d147
JuHee Kim <rlawngml59@gmail.com>
2022-10-25 15:36:55 +0900

    appfordog.app
    - 버그 수정
    - 프로젝트 완료

--- teamproject/mainapp/views.py
+++ teamproject/mainapp/views.py
@@ -51,13 +51,13 @@ def sort_m1_3(request):
 def sort_m1_4(request):
     context = ""
     if request.method == 'GET':
-        result_r1 = request.GET.get('result_r1',"")
+        result_r1 = request.GET.get('result_r1', "")
         category_id = request.GET.get('category_id', "")
         context = {
             'results': {
                 'result_r1': (result_r1 + " > ")
             },
-            'category_id':category_id
+            'category_id': category_id
         }
     return render(request, 'dogapp/sort_m1_4.html', context)
 
@@ -71,13 +71,13 @@ def sort_m1_5(request):
             'results': {
                 'result_r1': (result_r1 + " > ")
             },
-            'category_id':category_id
+            'category_id': category_id
         }
     return render(request, 'dogapp/sort_m1_5.html', context)
 
 
 def sort_r2(request):
-    context=""
+    context = ""
     if request.method == 'GET':
         result_r1 = request.GET.get('result_r1', "")
         result_m1 = request.GET.getlist("result_m1", "")
@@ -86,7 +86,7 @@ def sort_r2(request):
         context = {
             'results': {
                 'result_r1': result_r1,
-                'result_m1': (result_m1[0] + " 외 " + str(len(result_m1)-1) + "개 > "),
+                'result_m1': (result_m1[0] + " 외 " + str(len(result_m1) - 1) + "개 > "),
             },
             'category_id': category_id,
             'type_id': type_id
@@ -161,7 +161,6 @@ def sort_m2_3(request):
 
 
 def sort_end(request):
-
     if request.method == 'GET':
         result_r1 = request.GET.get('result_r1', "")
         category_id = request.GET.get('category_id', "")
@@ -195,7 +194,7 @@ def sort_end(request):
                     'result_r1': result_r1,
                     'result_m1': result_m1,
                     'result_r2': result_r2,
-                    'result_m2': (result_m2[0] + " 외 " + str(len(result_m2)-1) + "개")
+                    'result_m2': (result_m2[0] + " 외 " + str(len(result_m2) - 1) + "개"),
                 },
                 'category_id': category_id,
                 'type_id': type_id,
